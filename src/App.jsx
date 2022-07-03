e3eda218e749b9f8ff7e82f4abea47adc452b684
Pietraceci <pietracecily@gmail.com>
2022-07-03 17:31:13 -0400

    atualizando códigos

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
 import "./App.css";
+import Button from "./Button.jsx";
 
 function App(){
-    const planos = [
-        {
-            title: "Start",
-            price: "Free",
-            qnt_users: "1 User",
-            qnt_projects: "1 project"
-        },
-        {
-            title: "Basic",
-            price: "$19.99",
-            qnt_users: "5 Users",
-            qnt_projects: "20 Projects"
-        },
-        {
-            title: "Expert",
-            price: "$129.99",
-            qnt_users: "Unlimited Users",
-            qnt_projects: "Unlimited Users"
-        }
-    ];
-	return(
-        <div>
-            
+    return(
+        <div className="principal">
+            <Button title = "PREVIOUS" desc="JQUERY: DETECT UPDATE OF HTML ELEMENT"/>
+            <Button title = "NEXT" desc="RAILS + DEVISE WORKING WITH MULTIPLE MODELS"/>
         </div>
-    );
-
+    )
 }
 export default App;
